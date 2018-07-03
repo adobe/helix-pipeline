eab26aaa88c18aecf3847cd0c7312b3ce13542d7
Lars Trieloff <trieloff@adobe.com>
2018-07-03 10:18:57 +0000

    Turn pipeline from a function into a class

--- index.js
+++ index.js
@@ -12,6 +12,85 @@
 const _ = require('lodash/fp');
 const Promise = require('bluebird');
 
+class Pipeline {
+  constructor(constants = {}, logger) {
+    this.constants = constants;
+    this.logger = logger;
+    this.last = [];
+    this.pres = [];
+    this.posts = [];
+    this.oncef = null;
+  }
+
+  pre(f) {
+    this.pres.push(f);
+    return this;
+  }
+
+  post(f) {
+    this.posts.push(f);
+    return this;
+  }
+
+  when(predicate) {
+    if (this.last && this.last.length > 0) {
+      const lastfunc = this.last.pop();
+      const wrappedfunc = (args) => {
+        if (predicate(args)) {
+          return lastfunc(args, this.constants, this.logger);
+        }
+        return args;
+      };
+      this.last.push(wrappedfunc);
+    }
+    return this; 
+  }
+
+  unless(predicate) {
+    const inverse = (args) => !predicate(args);
+    return this.when(inverse);
+  }
+
+  once(f) {
+    this.oncef = f;
+    return this;
+  }
+
+  run(args = {}) {
+    // go over inner.pres (those that run before), inner.oncef (the function that runs once)
+    // and inner.posts (those that run after) – reduce using the merge function and return
+    // the resolved value
+    const prom = Promise.reduce(
+      [...this.pres, this.oncef, ...this.posts],
+      this.merge,
+      args,
+    ).then(v => v);
+
+    return prom;
+  }
+
+  merge(accumulator, currentfunction) {
+    // copy the pipeline payload into a new object
+    // to avoid modifications
+    const mergedargs = _.merge({}, accumulator);
+
+    // log the function that is being called
+    // and the parameters of the function
+    if (this.logger) {
+      this.logger.silly('processing ', { function: `${currentfunction}`, params: mergedargs });
+    }
+
+    return Promise.resolve(currentfunction(mergedargs, this.constants, this.logger))
+      .then((value) => {
+        const result = _.merge(accumulator, value);
+        if (this.logger) {
+          inner.logger.silly('received ', { function: `${currentfunction}`, result });
+        }
+        return result;
+      });
+  };
+}
+
 function attacher() {
   const inner = (args = {}, constants, logger) => {
     // save the second and third argument, they don't get modified
@@ -121,4 +200,4 @@ function attacher() {
   return inner;
 }
 
-module.exports = attacher;
+module.exports = Pipeline;
