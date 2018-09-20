e52cd903423ebd6a68d32594c6dd300b03f5bb4d
Lars Trieloff <trieloff@adobe.com>
2018-09-20 15:11:52 +0000

    regexp-based content pattern matching (for Layout Machine)

---
+++ src/utils/pattern-compiler.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+function compile(pattern) {
+  // console.log(pattern);
+  const expression = new RegExp(pattern
+    .replace(/(\w+)/g, '($1·)') // always match whole words
+    .replace(/ /g, '') // remove spaces
+    .toString());
+  // console.log(expression);
+  return expression;
+}
+
+function match(list, pattern) {
+  return compile(pattern).test(list.join('·'));
+}
+
+module.exports = {
+  compile,
+  match,
+};
