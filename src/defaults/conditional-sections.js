9105597477ea7e41c3a9b204ea1523784c0bfcdf
Lars Trieloff <trieloff@adobe.com>
2018-11-10 10:33:28 +0000

    draft: conditional sections

---
+++ src/defaults/conditional-sections.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+function strain({content}, {request, logger}) {
+  // this only works when there are multiple sections and a strain has been chosen
+  if (request.params && 
+      request.params.strain && 
+      content && 
+      content.sections && 
+      content.sections.length) {
+    const strain = request.params.strain;
+    const sections = content.sections;
+    logger.debug('Filtering sections not intended for strain ' + strain);
+    const remaining = sections.filter(section => {
+      return true;
+    });
+    logger.debug(remaining.length + 'Sections remaining');
+    return {
+      content: {
+        sections: remaining
+      }
+    }
+  }
+}
