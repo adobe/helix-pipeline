52ea9d98189a0d306bc636aec7477f34824162b7
Lars Trieloff <trieloff@adobe.com>
2018-11-12 15:03:26 +0000

    added test for modified (image-less) intro

--- test/testHTML.js
+++ test/testHTML.js
@@ -205,6 +205,64 @@ It works! {{project.name}} is up and running.
     assert.notEqual(500, result.response.status);
   });
 
+  it('html.pipe renders modified index.md from helix-cli correctly', async () => {
+    const result = await pipe(
+      ({ content }) => {
+        // this is the main function (normally it would be the template function)
+        // but we use it to assert that pre-processing has happened
+        assert.equal(content.title, 'Helix - {{project.name}}');
+        assert.equal(content.image, undefined);
+        assert.equal(content.intro, 'It works! {{project.name}} is up and running.');
+        // and return a different status code
+        return { response: { status: 201, body: content.html } };
+      },
+      {
+        content: {
+          body: `<!--
+~ Licensed to the Apache Software Foundation (ASF) under one or more
+~ contributor license agreements.  See the NOTICE file distributed with
+~ this work for additional information regarding copyright ownership.
+~ The ASF licenses this file to You under the Apache License, Version 2.0
+~ (the "License"); you may not use this file except in compliance with
+~ the License.  You may obtain a copy of the License at
+~
+~      http://www.apache.org/licenses/LICENSE-2.0
+~
+~ Unless required by applicable law or agreed to in writing, software
+~ distributed under the License is distributed on an "AS IS" BASIS,
+~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+~ See the License for the specific language governing permissions and
+~ limitations under the License.
+-->
+
+# Helix - {{project.name}}
+
+It works! {{project.name}} is up and running.
+
+## What's next?
+
+1. Try editing \`index.md\` and see what happens.
+2. Try editing \`src/html.htl\` to change the HTML output
+3. Try editing \`src/static/style.css\` to change the way this page looks
+
+## And then?
+
+4. Commit your changes: \`git commit -a\`
+5. Publish your project to GitHub: \`git add git remote add origin https://github.com/user/repo.git && git push\`
+6. Deploy the project: \`hlx deploy\`
+7. Make it visible to the world: \`hlx publish\``,
+        },
+      },
+      {
+        request: { params },
+        secrets,
+        logger,
+      },
+    );
+    assert.equal(result.error, undefined);
+    assert.notEqual(500, result.response.status);
+  });
+
   it('html.pipe complains when context is invalid', async () => {
     const result = await pipe(
       ({ content }) => ({ response: { status: 201, body: content.html } }),
