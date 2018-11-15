db2082c6465f46f474a07809a244aa4ace02704a
rofe <me@rofe.com>
2018-11-15 15:09:32 +0100

    Add XML pipeline #106

---
+++ src/defaults/xml.pipe.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+const { Pipeline } = require('../../index.js');
+const { log } = require('./default.js');
+
+const fetch = require('../html/fetch-markdown.js');
+const parse = require('../html/parse-markdown.js');
+const meta = require('../html/get-metadata.js');
+const type = require('../xml/set-content-type.js');
+const smartypants = require('../html/smartypants');
+const sections = require('../html/split-sections');
+const production = require('../utils/is-production');
+const dump = require('../utils/dump-context.js');
+const validate = require('../utils/validate');
+
+/* eslint no-param-reassign: off */
+
+const xmlpipe = (cont, payload, action) => {
+  action.logger = action.logger || log;
+  action.logger.log('debug', 'Constructing XML Pipeline');
+  const pipe = new Pipeline(action);
+  pipe
+    .every(dump).when(() => !production())
+    .every(validate).when(() => !production())
+    .before(fetch)
+    .before(parse)
+    .before(smartypants)
+    .before(sections)
+    .before(meta)
+    .once(cont)
+    .after(type);
+
+  action.logger.log('debug', 'Running XML pipeline');
+  return pipe.run(payload);
+};
+
+module.exports.pipe = xmlpipe;
---
+++ src/xml/set-content-type.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+function type({ response }, { logger }) {
+  // somebody already set a content type, keep as is
+  if (!(response && response.headers && response.headers['Content-Type'])) {
+    logger.debug('Setting content type header');
+    return {
+      response: {
+        headers: {
+          'Content-Type': 'application/xml',
+        },
+      },
+    };
+  }
+  logger.debug('Keeping existing content type header');
+  return {};
+}
+module.exports = type;
