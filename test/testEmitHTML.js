51e3076f34608cdf09da41edae44f8f09c955be2
Alexandre Capt <kptdobe@gmail.com>
2018-11-22 15:53:21 +0100

    [Regression] payload.content.children contains only one child #115 (#116)
    Fixes #115

---
+++ test/testEmitHTML.js
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* eslint-env mocha */
+const assert = require('assert');
+const winston = require('winston');
+const emit = require('../src/html/emit-html');
+
+const logger = winston.createLogger({
+  // tune this for debugging
+  level: 'debug',
+  // and turn this on if you want the output
+  silent: true,
+  format: winston.format.simple(),
+  transports: [new winston.transports.Console()],
+});
+
+describe('Test HTML emitter', () => {
+  it('emitter works with empty document', () => {
+    const out = emit({ content: { document: { body: {} } } }, { logger });
+    assert.deepEqual({
+      content: {
+        children: [],
+        html: '',
+      },
+    }, out);
+  });
+
+  it('emitter transforms the given body', () => {
+    const out = emit({
+      content: {
+        document: {
+          body: {
+            innerHTML: 'ab',
+            firstChild: {
+              childNodes: [{
+                outerHTML: 'a',
+              }, {
+                outerHTML: 'b',
+              }],
+            },
+          },
+        },
+      },
+    }, { logger });
+    assert.deepEqual({
+      content: {
+        children: ['a', 'b'],
+        html: 'ab',
+      },
+    }, out);
+  });
+});
