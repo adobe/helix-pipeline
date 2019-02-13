9d28475b60b76c9ed11efb896e0cc1bb111bcc70
Tobias Bocanegra <tripod@adobe.com>
2019-02-13 17:11:54 +0900

    fixing tests and updating docs

--- src/action.d.ts
+++ src/action.d.ts
@@ -1,21 +1,21 @@
 /*
- * Copyright 2018 Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy
- * of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
+  * Copyright 2018 Adobe. All rights reserved.
+  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License. You may obtain a copy
+  * of the License at http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software distributed under
+  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+  * OF ANY KIND, either express or implied. See the License for the specific language
+  * governing permissions and limitations under the License.
+  */
 
 /**
  * The Request Object used for Invoking OpenWhisk
  */
 export type RawRequest = {
   /**
-   * The headers of the request made to OpenWhisk/helix-simulator
+   * The headers of the request made to OpenWhisk/Petridish
    */
   headers?: {
     /**
@@ -37,7 +37,7 @@ export type RawRequest = {
     [k: string]: string;
   };
   /**
-   * The HTTP method of the request made to OpenWhisk/helix-simulator. Note: OpenWhisk converts all methods to lowercase.
+   * The HTTP method of the request made to OpenWhisk/Petridish. Note: OpenWhisk converts all methods to lowercase.
    */
   method?: string;
   /**
@@ -58,14 +58,8 @@ export type RawRequest = {
     ref?: string;
     /**
      * Path to the requested (Markdown) file
-     * @example "/general/index.md"
      */
     path?: string;
-    /**
-     * The request root path of the current strain.
-     * @example "/docs/api"
-     */
-    rootPath?: string;
     /**
      * The resolved strain (variant)
      */
@@ -77,7 +71,7 @@ export type RawRequest = {
       [k: string]: any;
     };
     /**
-     * All other parameters are interpreted as string.
+     * All other parameters are interpretet as string.
      */
     [k: string]: string;
   };
