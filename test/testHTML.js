7d3956ee742d905db294687b9b95d28b491686e3
rofe <github@rofe.com>
2019-06-06 14:29:08 +0200

    test(xss): make sure SANITIZE_DOM is actually true
    The Object.assign() statement with inversed params would only work if this test was run alone; if
    run with all tests above, the SANITIZE_DOM property would stay false (default in schema)
    
    fix #360

--- test/testHTML.js
+++ test/testHTML.js
@@ -713,7 +713,7 @@ ${context.content.document.body.innerHTML}`,
       },
       {
         request: { params },
-        secrets: Object.assign({ SANITIZE_DOM: true }, secrets),
+        secrets: Object.assign(secrets, { SANITIZE_DOM: true }),
         logger,
       },
     );
