1cfb476e47e91291de0fc11a7f22a35a260a3106
rofe <github@rofe.com>
2019-08-29 17:44:58 +0200

    fix(fetch): check if request.headers is undefined

--- src/html/fetch-markdown.js
+++ src/html/fetch-markdown.js
@@ -84,7 +84,7 @@ async function fetch(context, { secrets = {}, request, logger }) {
   };
 
   // if there is a github token, send it in the Authorization header
-  const token = secrets.GITHUB_TOKEN || request.headers['x-github-token'];
+  const token = secrets.GITHUB_TOKEN || (request.headers ? request.headers['x-github-token'] : '');
   if (token) {
     options.headers = options.headers || {};
     options.headers.Authorization = `token ${token}`;
