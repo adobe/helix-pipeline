c7ade6fd71568243daf1bb399fe243aa177a3051
rofe <github@rofe.com>
2019-10-23 11:56:21 +0200

    chore(pipeline): implemented feedback from PR

--- src/html/iconize.js
+++ src/html/iconize.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Adobe. All rights reserved.
+ * Copyright 2019 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -11,43 +11,46 @@
  */
 const visit = require('unist-util-visit');
 
-const regexp = /:([a-zA-Z0-9_]+):/g;
+const regexp = /:([a-z0-9_-]+):/gi;
 
 function iconize({ content }) {
   if (!content.mdast) {
     return;
   }
   visit(content.mdast, (node, index, parent) => {
-    if (node.type === 'text' && regexp.test(node.value)) {
-      let text = node.value;
-      const nodes = [];
-      text.match(regexp).forEach((match) => {
-        const matchIndex = text.indexOf(match);
-        if (matchIndex > 0) {
-          // add node with the text before the icon
+    if (node.type === 'text') {
+      const icons = node.value.match(regexp);
+      if (icons) {
+        let text = node.value;
+        const nodes = [];
+        icons.forEach((match) => {
+          const matchIndex = text.indexOf(match);
+          if (matchIndex > 0) {
+            // add node with the text before the icon
+            nodes.push({
+              type: 'text',
+              value: text.substring(0, matchIndex),
+            });
+          }
+          // add the icon node
+          nodes.push({
+            type: 'icon',
+            value: match.substring(1, match.length - 1),
+            code: match,
+          });
+          // truncate text
+          text = text.substring(matchIndex + match.length);
+        });
+        if (text.length > 0) {
+          // add final node with the rest of the text
           nodes.push({
             type: 'text',
-            value: text.substring(0, matchIndex),
+            value: text,
           });
         }
-        // add the icon node
-        nodes.push({
-          type: 'icon',
-          value: match.substring(1, match.length - 1),
-          code: match,
-        });
-        // truncate text
-        text = text.substring(matchIndex + match.length);
-      });
-      if (text.length > 0) {
-        // add final node with the rest of the text
-        nodes.push({
-          type: 'text',
-          value: text,
-        });
+        // replace original text node
+        parent.children.splice(index, 1, ...nodes);
       }
-      // replace original text node
-      parent.children.splice(index, 1, ...nodes);
     }
   });
 }
