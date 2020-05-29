28a7abfa47bf7ac105ca987e146297e08377febd
Raphael Wegmueller <github@rofe.com>
2020-05-29 14:38:28 +0200

    feat(pipe): move image handler into pipeline step (#737)

--- src/utils/mdast-to-vdom.js
+++ src/utils/mdast-to-vdom.js
@@ -20,7 +20,6 @@ const HeadingHandler = require('./heading-handler');
 const embed = require('./embed-handler');
 const link = require('./link-handler');
 const icon = require('./icon-handler');
-const image = require('./image-handler');
 const section = require('./section-handler');
 const types = require('../schemas/mdast.schema.json').properties.type.enum;
 
@@ -71,7 +70,6 @@ class VDOMTransformer {
     this.match('link', link(this._options));
     this.match('icon', icon(this._options));
     this.match('section', section(this._options));
-    this.match('image', image(this._options));
     this.match('html', (h, node) => {
       if (node.value.startsWith('<!--')) {
         return h.augment(node, {
